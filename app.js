a7ce028fa7b8d05a1ccf6bd4b152f05b1fdd92f0
Jatin Rathee <jatinrathee2018@gmail.com>
2020-04-09 16:41:34 +0530

    module pattern

---
+++ app.js
@@ -0,0 +1,37 @@
+var budgetController = (function() {
+
+    var x = 23;
+
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            return add(b);
+        }
+    }
+
+})();
+
+
+
+var UIControlller = (function() {
+    
+    // Some code
+
+})();
+
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIControlller);
